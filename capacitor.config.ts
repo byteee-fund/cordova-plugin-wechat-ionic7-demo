dbd43878231cbe1e6595aa107223f5666adf817f
Jason.z <281674669@qq.com>
2024-01-29 11:57:24 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'cordova-plugin-wechat-ionic7-demo',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
